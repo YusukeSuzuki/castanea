1fd9b9f0d50aee2d1b73009f8c7d0f9340c9c6ba
Yusuke Suzuki <yusuke@geekfield.jp>
2017-04-18 16:59:47 +0900

    add weight normalize

--- castanea/normalize.py
+++ castanea/normalize.py
@@ -1,6 +1,50 @@
 import tensorflow as tf
 
-def weight_normalize():
-    pass
+def normalize_weight_for_conv2d(w, g_init, var_device='/cpu:0'):
+    '''
+    weight normalization for conv2d
 
+    @param w conv2d weight to be normalized
+    @param var_device device name for variable placement
+    @return normalized weight
+    '''
+    with tf.variable_scope('weight_normalize'):
+        shape = w.get_shape().as_list()
+        with tf.device(var_device):
+            g = tf.get_variable(name='g', shape=[1,1,1,shape[3]],
+                initializer=tf.constant_initializer(g_init))
+
+        return g * tf.nn.l2_normalize(w, [0,1,2]) 
+
+
+def normalize_weight_for_conv2d_transpose(w, g_init, var_device='/cpu:0'):
+    '''
+    weight normalization for conv2d transpose
+
+    @param w conv2d transpose weight to be normalized
+    @param var_device device name for variable placement
+    @return normalized weight
+    '''
+    with tf.variable_scope('weight_normalize'):
+        shape = w.get_shape().as_list()
+        with tf.device(var_device):
+            g = tf.get_variable(name='g', shape=[1,1,shape[2],1],
+                initializer=tf.constant_initializer(g_init))
+
+        return g * tf.nn.l2_normalize(w, [0,1,3]) 
+
+def normalize_weight_for_linear(w, g_init, var_device='/cpu:0'):
+    '''
+    weight normalization for linear layer
+
+    @param w linear weight to be normalized
+    @param var_device device name for variable placement
+    @return normalized weight
+    '''
+    with tf.variable_scope('weight_normalize'):
+        shape = w.get_shape().as_list()
+        with tf.device(var_device):
+            g = tf.get_variable(name='g', shape=[1,shape[1]],
+                initializer=tf.constant_initializer(math.log(stddev)))
+        return = g * tf.nn.l2_normalize(w, [0]) 
 
