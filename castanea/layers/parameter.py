ae2a792b3fd46fbce1fc73be198c1b3c99c077c9
Yusuke Suzuki <yusuke@geekfield.jp>
2017-09-10 00:13:57 +0900

    add parameter 'training' to layer parameter class

--- castanea/layers/parameter.py
+++ castanea/layers/parameter.py
@@ -3,7 +3,8 @@ import tensorflow as tf
 class LayerParameter:
     def __init__(self,
             padding='SAME', with_bias=False, with_weight_normalize=False, rectifier=None,
-            with_batch_normalize=False, var_device=None, var_scope_default_name=None):
+            with_batch_normalize=False, var_device=None, var_scope_default_name=None,
+            training=False):
         self.padding = padding
         self.with_bias = with_bias
         self.with_weight_normalize = with_weight_normalize
@@ -11,4 +12,5 @@ class LayerParameter:
         self.rectifier = rectifier
         self.var_device = var_device
         self.var_scope_default_name = var_scope_default_name
+        self.training=training
 
